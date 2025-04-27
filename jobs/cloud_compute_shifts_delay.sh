c81c8e2917c230c26d158de4a5661eb31759d783
Štěpán Zapadlo <stepan.zapadlo@gmail.com>
2025-04-27 06:51:39 +0200

    Split job the job to the delay and non-delay part Move to praha1 storage (brno2 seems down)

--- jobs/cloud_compute_shifts_delay.sh
+++ jobs/cloud_compute_shifts_delay.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#PBS -N julia_compute_shift_heatmap
+#PBS -N julia_compute_delay_shift_heatmap
 #PBS -l select=1:ncpus=20:mem=36gb:scratch_local=150gb
-#PBS -l walltime=10:00:00 
+#PBS -l walltime=16:00:00 
 # The 4 lines above are options for scheduling system: job will run 10 hour at maximum, 1 machine with 20 processors + 36gb RAM memory + 150gb scratch memory are requested
 
 # define a DATADIR variable: directory where the input files are taken from and where output will be copied to
-DATADIR=/storage/brno2/home/sceptri/ # substitute username and path to to your real username and path
+DATADIR=/storage/praha1/home/sceptri/ # substitute username and path to to your real username and path
 
 # append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of node it is run on and the path to a scratch directory
 # this information helps to find a scratch directory in case the job fails and you need to remove the scratch directory manually 
@@ -33,11 +33,9 @@ julia --project="./sections" -e 'using Pkg; Pkg.instantiate()'
 
 # if the calculation ends with an error, issue error message an exit
 julia -t $PBS_NCPUS --project="./sections" scripts/delay_computation_metacentrum.jl || { echo >&2 "Calculation ended up erroneously (with a code $?) !!"; exit 3; }
-julia -t $PBS_NCPUS --project="./sections" scripts/no_delay_computation_metacentrum.jl || { echo >&2 "Calculation ended up erroneously (with a code $?) !!"; exit 3; }
 
 # move the output to user's DATADIR or exit in case of failure
 cp data/delay_computation_metacentrum.jld2 $DATADIR/Projects/masters-thesis/data/delay_computation_metacentrum_$PBS_JOBID.jld2 || { echo >&2 "Copying data failed (with a code $?) !!"; exit 4; }
-cp data/no_delay_computation_metacentrum.jld2 $DATADIR/Projects/masters-thesis/data/no_delay_computation_metacentrum_$PBS_JOBID.jld2 || { echo >&2 "Copying data failed (with a code $?) !!"; exit 4; }
 
 # clean the SCRATCH directory
 clean_scratch
